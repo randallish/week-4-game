585d51ac9203586e52121ad21954c81e4ae66523
Randall Ishino <rishino24@gmail.com>
2017-12-16 02:27:20 -0800

    css added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,7 @@ var redCrystal = Math.floor((Math.random() * 12) + 1);
 var blueCrystal = Math.floor((Math.random() * 12) + 1);
 var yellowCrystal = Math.floor((Math.random() * 12) + 1);
 var greenCrystal = Math.floor((Math.random() * 12) + 1); 
-console.log(redCrystal);
+console.log(redCrystal,blueCrystal,yellowCrystal,greenCrystal);
 // wins,losses, and score counters
 var wins = 0;
 var losses = 0;
@@ -22,27 +22,42 @@ $("#random-number").text("Your random number is: " + randomNumber);
 $("#total-score").text("Your total score is:" + totalScore);
 
 
-//
-
-
-// resets game
-function resetGame() {
-    randomNumber = Math.floor((Math.random() * 120 - 19) + 19);
-    redCrystal = Math.floor((Math.random() * 12) + 1);
-    blueCrystal = Math.floor((Math.random() * 12) + 1);
-    yellowCrystal = Math.floor((Math.random() * 12) + 1);    
-    greenCrystal = Math.floor((Math.random() * 12) + 1); 
-    totalScore = 0;   
-    $("#random-number").text("Your random number is: " + randomNumber);
-    $("#total-score").text("Your total score is: " + totalScore);
-};
-
 // click event for the red crystal
 $("#red-crystal").on("click",function() {
     totalScore = redCrystal + totalScore;
     $("#total-score").text("Your total score is: " + totalScore);
     console.log(totalScore);
+    checkWinOrLoss();
+})
 
+//click event for blue crystal 
+$("#blue-crystal").on("click",function() {
+    totalScore = blueCrystal + totalScore;
+    $("#total-score").text("Your total score is: " + totalScore);
+    console.log(totalScore);
+    checkWinOrLoss();
+
+})
+
+$("#green-crystal").on("click",function() {
+    totalScore = greenCrystal + totalScore;
+    $("#total-score").text("Your total score is: " + totalScore);
+    console.log(totalScore);
+    checkWinOrLoss();
+})
+
+$("#yellow-crystal").on("click",function() {
+    totalScore = yellowCrystal + totalScore;
+    $("#total-score").text("Your total score is: " + totalScore);
+    console.log(totalScore);
+    checkWinOrLoss();
+
+})
+
+
+
+function checkWinOrLoss() {
+    
     if (totalScore == randomNumber){
         wins++;
         $("#wins").text("Wins: "  + wins);
@@ -56,64 +71,16 @@ $("#red-crystal").on("click",function() {
         alert("You lose!")
         resetGame();
     }
-})
+}
 
-//click event for blue crystal 
-$("#blue-crystal").on("click",function() {
-    totalScore = blueCrystal + totalScore;
+// resets game
+function resetGame() {
+    randomNumber = Math.floor((Math.random() * 120 - 19) + 19);
+    redCrystal = Math.floor((Math.random() * 12) + 1);
+    blueCrystal = Math.floor((Math.random() * 12) + 1);
+    yellowCrystal = Math.floor((Math.random() * 12) + 1);    
+    greenCrystal = Math.floor((Math.random() * 12) + 1); 
+    totalScore = 0;   
+    $("#random-number").text("Your random number is: " + randomNumber);
     $("#total-score").text("Your total score is: " + totalScore);
-    console.log(totalScore);
-
-    if (totalScore == randomNumber){
-        wins++;
-        $("#wins").text("Wins: " + wins);
-        alert("You win!");
-        resetGame();
-    }
-
-    else if (totalScore > randomNumber){
-        losses++;
-        $("#losses").text("Losses: " + losses);
-        alert("You lose!")
-        resetGame();
-    }
-})
-
-$("#green-crystal").on("click",function() {
-    totalScore = greenCrystal + totalScore;
-    $("#total-score").text("Your total score is: " + totalScore);
-    console.log(totalScore);
-
-    if (totalScore == randomNumber){
-        wins++;
-        $("#wins").text("Wins: " + wins);        
-        alert("You win!");
-        resetGame();
-    }
-
-    else if (totalScore > randomNumber){
-        losses++;
-        alert("You lose!");
-        resetGame();
-    }
-})
-
-$("#yellow-crystal").on("click",function() {
-    totalScore = yellowCrystal + totalScore;
-    $("#total-score").text("Your total score is: " + totalScore);
-    console.log(totalScore);
-
-    if (totalScore == randomNumber){
-        wins++;
-        $("#wins").text("Wins: " + wins);        
-        alert("You win!");
-        resetGame();
-    }
-
-    else if (totalScore > randomNumber){
-        losses++;
-        $("#losses").text("Losses: " + losses);        
-        alert("You lose!");
-        resetGame();
-    }
-})
+}
